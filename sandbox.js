635980f18c50582e807bed468093ffe4ce4002d0
devHksingh <hksingh.dev@gmail.com>
2024-09-21 10:41:05 +0530

    add JS functionality to  nav Link in Desktop mode

--- sandbox.js
+++ sandbox.js
@@ -4,6 +4,36 @@ const mobileMenuCloseElement = document.querySelector(".mobile-menu-close-btn")
 const mobileMenuOpenElement = document.querySelector(".mobile-menu-btn")
 const mobileMenuElement = document.querySelector(".mobile-menu")
 const mobileMenuLinkElement = document.querySelector(".mobile-menu ul li a")
+// const deskMenuElement = document.querySelector('.desktop-menu')
+const deskMenuLiAElements = document.querySelectorAll('.desktop-menu-li a')
+// const menuATagElement = document.querySelector('.desktop-menu-li a')
+// const deskMenu1Element = document.querySelector('#d-menu-1')
+// const deskMenu2Element = document.querySelector('#d-menu-2')
+// const deskMenu3Element = document.querySelector('#d-menu-3')
+// const deskMenu4Element = document.querySelector('#d-menu-4')
+// const deskMenu5Element = document.querySelector('#d-menu-5')
+
+// const windowPathName = window.location.pathname
+// deskMenuLiElements.forEach(navLink =>{
+//     console.log(navLink);
+//     // if(navLink.href.includes(windowPathName)){
+//     //     navLink.classList.add('menu-active')
+//     //     console.log(navLink.href);
+        
+//     // }else{
+//     //     navLink.classList.remove('menu-active')
+//     // }
+// })
+
+
+function placeBorder(id){
+    deskMenuLiAElements.forEach((a)=>{
+        a.classList.remove('menu-active');
+    })
+    console.log(id);
+    id.classList.add('menu-active')
+    
+}
 
 const openMenu = ()=>{
     mobileMenuOpenElement.style.display="none"
@@ -31,4 +61,17 @@ mobileMenuLinkElement.addEventListener('click',(e)=>{
     mobileMenuCloseElement.style.display="none"
 })
 
-console.log(mobileMenuLinkElement);
+// console.log(deskMenuLiElement);
+
+// deskMenuElement.addEventListener('click',function(e){
+//     console.log("a click");
+//     console.log(deskMenuElement);
+//     console.log(deskMenuLiHighperLinkElement);
+//     console.log(menuATagElement);
+//     console.log(e);
+//     // menuATagElement.style.color ="yellow"
+
+    
+// })
+
+
